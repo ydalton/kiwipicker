729595dec599d64dbaa23150efe00516330d9420
ydalton <113905856+ydalton@users.noreply.github.com>
2023-06-01 00:54:05 +0200

    Early prototype

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.scss'
 import Station from "./Components";
+import stat from "./stations";
+
 
 function App() {
-  let station = <Station text="Google" link="https://www.google.com"/>
-  const children = this.myData.map((val) => (
-    {station}
-  ));
-  let app = <div className="App" class="row justify-content-center">
-              {children}
-            </div>;
-  return app;
+  let stations = stat['stations'];
+  return (
+    <div className="App" class="row justify-content-center">
+      {stations.map(station => (
+        <Station text={station['name']} link={station['link']}/>
+      ))}
+    </div>
+  );
 }
 
 export default App;
