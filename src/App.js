b3472c10d20fac000b64671b5abae36e9fbd9eb6
ydalton <yussef.dalton@gmail.com>
2023-07-19 00:25:52 +0200

    Implement import button, almost done with save button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+// Copyright (c) 2023 ydalton
+
 import './App.scss'
 import Station from "./Station";
-import stat from "./stations";
+import defaultStations from "./defaultStations";
+import { useState, changeEvent } from "react";
 
 function AddButton() {
   return (
-    <a href="#" class="add text-center col-11 col-lg-2 p m-2">
+    <a href="#"
+      title="Click here to add a station!"
+      className="add text-center col-11 col-lg-2 p m-2">
       <p>
         +
       </p>
@@ -13,17 +18,61 @@ function AddButton() {
 }
 
 function App() {
-  let stations = stat['stations'];
-  if(stations == null)
-    return <p>No stations found. Try loading a different file</p>;
-  return (
-    <div className="App row justify-content-center">
+  let appContents;
+  let [stations, setStations] = useState(null);
+  let fileReader;
+
+  const fileReadHandler = (e) => {
+    const content = fileReader.result;
+    const result = JSON.parse(content);
+    setStations(result['stations']);
+    console.log(result);
+  }
+
+  const changeHandler = (file) => {
+    fileReader = new FileReader();
+    fileReader.onloadend = fileReadHandler;
+    fileReader.readAsText(file);
+  }
+
+  const saveHandler = () => {
+    // hack:
+    let stringed =`{"stations": ${JSON.stringify(stations)}}`;
+    console.log(stringed);
+  }
+
+  appContents = (stations == null) ? (
+    <div className="p-4">
+      <p>No stations found. Try loading a stations file.</p>
+    </div>
+  ) : (
+    <div id="stations" className="row justify-content-center">
       {stations.map(station => (
         <Station key={station['link']}
-                 text={station['name']}
-                 link={station['link']}/>
+                text={station['name']}
+                link={station['link']}/>
       ))}
       <AddButton/>
+    </div>
+  );
+
+  return (
+    <div className="App ">
+      <h1 className="text-center p-4">Kiwipicker</h1>
+      <div className="file-upload p-4 m-4">
+        <p>Enter a file: </p>
+        <input type="file"
+               id="file"
+               accept=".json"
+               onChange={e => changeHandler(e.target.files[0])}
+               className="m-2"/>
+        <br/>
+        <br/>
+        <button onClick={saveHandler} className="m-2">
+          Save file
+        </button>
+      </div>
+      {appContents}
     </div>
   );
 }
--- src/Station.js
+++ src/Station.js
@@ -1,6 +1,8 @@
 function Station(props) {
   return (
-    <a href={props.link} class="station text-center col-11 col-lg-2 p m-2">
+    <a href={props.link}
+      title={props.text}
+      className="station text-center col-11 col-lg-2 p m-2">
       <p>
         {props.text}
       </p>
