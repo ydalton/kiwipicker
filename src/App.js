04db19d0abac5350ea4818ae30af54f68bb9a8fa
ydalton <yussef.dalton@gmail.com>
2023-07-19 19:42:00 +0200

    Add save function, add station function

--- src/App.js
+++ src/App.js
@@ -1,52 +1,95 @@
 // Copyright (c) 2023 ydalton
 
 import './App.scss'
-import Station from "./Station";
-// import defaultStations from "./defaultStations";
+import Station from "./components/Station";
 import { useState } from "react";
 
-function AddButton() {
-  return (
-    <a href="#"
-      title="Click here to add a station!"
-      className="add text-center col-11 col-lg-2 p m-2">
-      <p>
-        +
-      </p>
-    </a>
-  );
-}
-
-function App() {
+export default function App() {
   let appContents;
   let [stations, setStations] = useState(null);
   let fileReader;
+  let [popupVisible, isPopupVisible] = useState(false);
+  let stationName, stationLink;
 
   const fileReadHandler = (e) => {
     const content = fileReader.result;
-    let result;
-
+    const res = JSON.parse(content);
     try{
-      result = JSON.parse(content);
-      setStations(result['stations']);
+      setStations(res['stations']);
     } catch {
       setStations(-1);
     }
-    console.log(result);
+    console.log(res['stations']);
+  }
+
+  function AddButton() {
+    return <Station text="+"
+                    onclick={() => isPopupVisible(true)}
+                    link="#"/>;
+  }
+
+  function Popup() {
+    let output;
+
+    const addHandler = (e) => {
+      console.log("Not implemented, sorry.");
+      setStations(arr => [...arr, {name: stationName, link: stationLink}]);
+      isPopupVisible(false);
+    }
+
+    if(popupVisible)
+      output = (
+        <div id="bg-shadow">
+          <div className="popup p-4">
+            <p>Add new station</p>
+            <form onSubmit={addHandler}>
+              <label>Name</label>
+              <input className="m-1"
+                     type="text"
+                     name="name"
+                     onChange={(e) => stationName = e.target.value}
+                     id="name"/>
+              <br/>
+              <label>Link</label>
+              <input className="m-1"
+                     type="text"
+                     name="link"
+                     onChange={(e) => stationLink = e.target.value}
+                     id="link"/>
+              <br/>
+              <input type="submit" value="Submit" />
+            </form>
+            <a id="close-btn" href="#" onClick={() => isPopupVisible(false)}>x</a>
+          </div>
+        </div>
+      );
+    return output;
   }
 
   const changeHandler = (file) => {
-    if(file == null)
+    if(file == null) {
+      console.log("changeHandler: file not loaded or is empty, bailing...");
       return;
+    }
     fileReader = new FileReader();
     fileReader.onloadend = fileReadHandler;
     fileReader.readAsText(file);
   }
 
   const saveHandler = () => {
-    // hack:
+    if(stations == null) {
+      console.log("saveHandler: file not loaded or is empty, bailing...");
+      return;
+    }
     let stringed =`{"stations": ${JSON.stringify(stations)}}`;
-    console.log(stringed);
+    // hack, but it works
+    let element = document.createElement('a');
+    const file = new Blob([stringed], {type: "application/json;charset=utf-8"});
+    element.href = URL.createObjectURL(file);
+    element.download = "stations.json";
+    document.body.appendChild(element);
+    element.click();
+    element.remove();
   }
 
   switch(stations) {
@@ -61,7 +104,7 @@ function App() {
         </div>
       );
     } break;
-    default: {
+    default:
       appContents = (
         <div id="stations" className="row justify-content-center">
           {stations.map(station => (
@@ -72,14 +115,14 @@ function App() {
           <AddButton/>
         </div>
       );
-    } break;
+    break;
   }
 
   return (
     <div className="App ">
       <h1 className="text-center p-4">Kiwipicker</h1>
       <div className="file-upload p-4 m-4">
-        <p>Enter a file: </p>
+        <p>Load stations file: </p>
         <input type="file"
                id="file"
                accept=".json"
@@ -91,8 +134,7 @@ function App() {
         </button>
       </div>
       {appContents}
+      <Popup />
     </div>
   );
 }
-
-export default App;
--- src/components/Station.js
+++ src/components/Station.js
@@ -2,6 +2,7 @@ function Station(props) {
   return (
     <a href={props.link}
       title={props.text}
+      onClick={props.onclick}
       className="station text-center col-11 col-lg-2 p m-2">
       <p>
         {props.text}
